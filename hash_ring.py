2acf770f5f660fa5f7db03d644b598824da52fef
Manvi Gupta <maanvigupta1620@gmail.com>
2026-02-13 17:18:45 +0530

    Refactor consistent hashing
    Refactor consistent hashing implementation to use a dictionary and a list for managing nodes and their hashes instead of SortedDict.

--- hash_ring.py
+++ hash_ring.py
@@ -1,12 +1,14 @@
-from sortedcontainers import SortedDict
+import bisect
 from hash_function import HashFunction
 
 
 class ConsistentHashing:
     def __init__(self, num_replicas=3):
         self.num_replicas = num_replicas
-        self.ring = SortedDict()
+        self.ring = {}
+        self.sorted_keys = []
         self.nodes = set()
+        self.node_hashes = {}
 
     def _prepare_key(self, node, index):
         return f"{node}_{index}"
@@ -16,11 +18,18 @@ class ConsistentHashing:
             return
 
         self.nodes.add(node)
+        self.node_hashes[node] = []
 
         for i in range(self.num_replicas):
             vnode_key = self._prepare_key(node, i)
             hash_value = HashFunction.hash(vnode_key)
+
+            while hash_value in self.ring:
+                hash_value += 1
+
+            bisect.insort(self.sorted_keys, hash_value)
             self.ring[hash_value] = node
+            self.node_hashes[node].append(hash_value)
 
     def remove_node(self, node):
         if node not in self.nodes:
@@ -28,22 +37,24 @@ class ConsistentHashing:
 
         self.nodes.remove(node)
 
-        for i in range(self.num_replicas):
-            vnode_key = self._prepare_key(node, i)
-            hash_value = HashFunction.hash(vnode_key)
-
+        for hash_value in self.node_hashes.get(node, []):
             if hash_value in self.ring:
                 del self.ring[hash_value]
+                index = bisect.bisect_left(self.sorted_keys, hash_value)
+                if index < len(self.sorted_keys) and self.sorted_keys[index] == hash_value:
+                    self.sorted_keys.pop(index)
+
+        del self.node_hashes[node]
 
     def get_node(self, key):
-        if not self.ring:
+        if not self.sorted_keys:
             return None
 
         hash_value = HashFunction.hash(key)
+        idx = bisect.bisect_left(self.sorted_keys, hash_value)
 
-        idx = self.ring.bisect_left(hash_value)
-
-        if idx == len(self.ring):
+        if idx == len(self.sorted_keys):
             idx = 0
 
-        return self.ring[self.ring.keys()[idx]]
+        return self.ring[self.sorted_keys[idx]]
+
